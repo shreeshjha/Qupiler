c40f5f644d542053a517ea98debdf442398506da
SamarthBhatia <samarthbhatia700@gmail.com>
2025-03-24 20:41:28 +0100

    feat: modified simplifiedAST generator

--- backend/json_to_simplified_ast.cpp
+++ backend/json_to_simplified_ast.cpp
@@ -297,7 +297,148 @@ std::unique_ptr<ASTNode> from_dict(const json& data) {
     std::optional<int> addr = (data.contains("addr") && !data["addr"].is_null()) ? std::make_optional(data["addr"].get<int>()) : std::nullopt;
 
     // Dispatch on the "kind" field
+    if (kind == "Assignment"){
+      std::string type=data.at("type").get<std::string>();
+      bool isNonBlocking=data.at("isNonBlocking").get<bool>();
+      // Recursive calls to convert left/right
+      auto left=from_dict(data.at("left"));
+      auto right=from_doct(data.at("right"));
+      //Construct an Assignment
+      return std::make_unique<Assignment>(kind,type,std::move(left),isNonBlocking,name,addr);
+    } 
+    
+    else if (kind == "BinaryOp"){
+      std::string type=data.at("type").get<std::string>();
+      std::string op=data.at("op").get<std::string>();
+      auto left=from_dict(data.at("left"));
+      auto right=from_dict(data.at("right"));
+      return std::make_unique<BinaryOp>(kind,type,op,std::move(left),std::move(right),name,addr);
+    } 
+    
+    else if (kind == "UnaryOp"){
+      std::string type=data.at("type").get<std::string>();
+      std::string op=data.at("op").get<std::string>();
+      auto operand=from_dict(data.at("operand"))l
+      return std::make_unique<UnaryOp>(kind,type,op,std::move(operand),name,addr)l
+    }
+
+    else if (kind == "CompilationUnit"){
+      return std::make_unique<CompilationUnit>(kind,name,addr);
+    }
+
+    else if (kind == "ContinuousAssign"){
+      auto assignment=from_dict(data.at("assignment"));
+      return std::make_unique<ContinuousAssign>(kind,std::move(assignment),name,addr);
+    }
+
+    else if (kind == "Conversion"){
+      std::string type=data.at("type").get<std::string>();
+      auto operand=from_dict(data.at("operand"));
+      if (data.contains("constant")){
+        std::string constant=data.at("constant").get<std::string>();  
+        return std::make_unique<Conversion>(kind,type,std::move(operand));
+      } 
+      else{
+        return std::make_unique<Conversion>(kind,type,std::move(operand),name,addr);    
+      }
+    }
+
+    else if (kind == "Instance"){
+      auto body=from_dict(data.at("body"));
+      return std::make_unique<Instance>(kind,std::move(body),name,addr);
+    }
+
+    else if (kind == "InstanceBody"){
+      auto members=from_dict_list(data.at("members"));
+      std::string declaration=data.at("definiton").get<std::string>();
+      return std::make_unique<InstanceBody>(kind,members,definiton,name,addr);
+    }
+
+    else if (kind == "NamedValue"){
+      std::string type=data.at("type").get<std::string>();
+      std::string symbol=data.at("symbol").get<std::string>();
+      std::optional<std::string> constant=(data.contains("constant") && !data["constant"].is_null()) ? std::make_optional(data["constant"].get<std::string()) : std::nullopt;
+      return std::make_unique<NamedValue>(kind,type,symbol,constant,name,addr);
+    }
+
+    else if (kind == "Net"){
+      std::string type=data.at("type").get<std::string>();
+      auto netType=from_dict(data.at("netType"));
+      return std::make_unique<Net>(kind,type,std::move(netType),name,addr);
+    }
+    
+    else if (kind == "NetType"){
+      std::string type=data.at("type").get<std::string>();
+      return std::make_unique<NetType>(kind,type,name,addr);
+    }
+
+    else if (kind == "Port"){
+      std::string type=data.at("type").get<std::string>();
+      std::string direction=data.at("direction").get<std::string>();
+      std::string internalSymbol=data.at("internalSymbol").get<std::string>();
+      return std::make_unique<Port>(kind,type,direction,internalSymbol,name,addr);
+    }
+    
+    else if (kind == "PrimitiveInstance"){
+      std::string primitiveType=data.at("primitiveType").get<std::string>();
+      auto ports=from_dict_list(data.at("ports"));
+      return std::make_unique<PrimitiveInstance>(kind,primitiveType,ports,name,addr);
+    }
+
+    else if (kind == "Root"){
+      auto members=from_dict_list(data.at("members"));
+      return std::make_unique<Root>(kind,members,name,addr);
+    }
+
+    else if (kind == "Variable"){
+      std::string type=data.at("type").get<std::string>();
+      std::string lifetime=data.at("lifetime").get<std::string>();
+      return std::make_unique<Variable>(kind,type,lifetime,name,addr);
+    }
+
+    else if (kind == "ProceduralBlock"){
+      auto body=from_dict(data.at("body"));
+      std::string procedureKind=data.at("procedureKind").get<std::string>();
+      return std::make_unique<ProceduralBlock>(kind,std::move(body),procedureKind, name, addr);
+    }
+
+    else if (kind == "Block"){
+      std::string blockKind=data.at("blockKind").get<std::string>();
+      auto body=from_dict(data.at("body"));
+      return std::make_unique<Block>(kind,blockKind,std::move(body),name,addr);
+    }
+
+    else if (kind == "ExpressionStatement"){
+      auto expr=from_dict(data.at("expr"));
+      return std::make_unique<ExpressionStatement>(kind,std::move(expr),name,addr);
+    }
+
+    else if (kind == "List"){
+      auto listNodes=from_dict_list(data.at("list"));
+      return std::make_unique<ListNode>(kind,listNodes,name,addr);
+    }
+
+    else if (kind == "IntegerLiteral"){
+      std::string type=data.at("type").get<std::string>();
+      std::string value=data.at("value").get<std::string>();
+      std::string constant=data.at("constant").get<std::string>();
+      return std::make_unique<IntegerLiteral>(kind,type,value,constant,name,addr);
+    }
+
+    else{
+      throw std::runtime_error("Unknown kind: " + kind);
+    }
 }
+
+// Helper that converts a JSON array to a vector of ASTNode pointers
+std::vector<std::unique_ptr<ASTNode>> from_dict_list(const json& arr){
+  std::vector<std::unique_ptr>> nodes;
+  for (const auto& item:arr){
+    nodes.push_back(from_dict(item));
+  }
+  return nodes;
+}
+
 
 
 int main() {
