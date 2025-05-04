16e3f2ca0c59bd3efb8d98a1d73175a69883c977
SamarthBhatia <samarthbhatia700@gmail.com>
2025-05-04 19:11:36 +0200

    fix:EliminateAdjacentCx.cpp fixed

--- backend/passes/EliminateAdjacentCx.cpp
+++ backend/passes/EliminateAdjacentCx.cpp
@@ -4,7 +4,9 @@
 // return number of removals
 //
 int eliminateAdjacentCx(std::string &content) {
-    std::regex pattern(R"((\s*)(q\.cx\s+(%\w+)\s*,\s*(%\w+))(\s*)\n\1(q\.cx\s+\3\s*,\s*\4)(\s*))");
+    // std::regex pattern(R"((\s*)(q\.cx\s+(%\w+)\s*,\s*(%\w+))(\s*)\n\1(q\.cx\s+\3\s*,\s*\4)(\s*))");
+    std::regex pattern(R"((\s*)(q\.cx\s+(%\w+\[\d+\])\s*,\s*(%\w+\[\d+\]))(\s*)\n\1\2)");
+
     int count = 0;
     std::smatch m;
     std::string tmp = content;
